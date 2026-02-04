b5c4827dc37e93fdd282097b0ddc703c1240eed0
Noah <nnnoahhhill@gmail.com>
2026-02-03 16:48:09 -0800

    Add footglove favicon files to footglove page metadata

--- app/goods/footgloving/layout.tsx
+++ app/goods/footgloving/layout.tsx
@@ -11,6 +11,19 @@ export async function generateMetadata(): Promise<Metadata> {
   
   return {
     title: "buy them",
+    icons: {
+      icon: [
+        { url: '/footglove_favicon/favicon.ico', sizes: 'any' },
+        { url: '/footglove_favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+        { url: '/footglove_favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      ],
+      apple: [
+        { url: '/footglove_favicon/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+      ],
+      other: [
+        { rel: 'manifest', url: '/footglove_favicon/site.webmanifest' },
+      ],
+    },
     openGraph: {
       title: "buy them",
       images: [previewImage],
